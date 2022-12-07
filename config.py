3cff89afc090596679f683b20f121c72094e7c01
Masabito <117425999+Masabito@users.noreply.github.com>
2022-12-07 02:38:08 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_HASH = os.environ.get('api_hash','5646106d87debbd9becfb7f13856763f')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = ('Krixt0')
-PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KJGJJJYJJJLJFJYJDJGJYJJJJJJJRJDJLJDJLJ'))
+PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KGGGJGYGJGLGFGYGDGGGYGKGFGLGRGCGLGGGLG'))
 
 if PROXY:
   print(f'Proxy {PROXY.as_dict_proxy()}')
